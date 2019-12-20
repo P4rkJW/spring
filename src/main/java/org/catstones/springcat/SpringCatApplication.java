dc56260ff9c6d2a305ff45c5918ea89f255049b1
An Nyeong <me@annyeong.me>
2019-12-20 21:16:39 +0900

    init

---
+++ src/main/java/org/catstones/springcat/SpringCatApplication.java
@@ -0,0 +1,13 @@
+package org.catstones.springcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/catstones/springcat/SpringCatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.catstones.springcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
